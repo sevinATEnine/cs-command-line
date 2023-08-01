0260f9e0d38f32ad9901556412d2876471a7a048
sevinATEnine <106199818+sevinATEnine@users.noreply.github.com>
2023-08-01 08:52:46 -0600

    Changed a lot

--- terminal.js
+++ terminal.js
@@ -1,3 +1,30 @@
+function forceDownload(blob, filename) {
+  var a = document.createElement('a');
+  a.download = filename;
+  a.href = blob;
+  document.body.appendChild(a);
+  a.click();
+  a.remove();
+}
+
+// Current blob size limit is around 500MB for browsers
+function downloadResource(url, filename) {
+  if (!filename) filename = url.split('\\').pop().split('/').pop();
+  fetch(url, {
+      headers: new Headers({
+        'Origin': location.origin
+      }),
+      mode: 'cors'
+    })
+    .then(response => response.blob())
+    .then(blob => {
+      let blobUrl = window.URL.createObjectURL(blob);
+      forceDownload(blobUrl, filename);
+    })
+    .catch(e => console.log(e));
+}
+
+
 let names = {
   "c@d3N":"ThatGuyOverThere",
   "$|m0n":"TacoMan",
@@ -70,6 +97,10 @@ function doCommand() {
     }
     case "admin": {
       output.innerHTML="<img src='./rickroll.gif'>";
+      break;
+    }
+    case "download": {
+      downloadResource(cmdSplit[1]);
       break;
     }
     case "lockdown": {
